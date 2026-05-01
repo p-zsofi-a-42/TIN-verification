34e7d8f491a0ab56e4cd6fc308c1bd4212093adc
p-zsofi-a-@-home <177267292+p-zsofi-a-42@users.noreply.github.com>
2026-05-01 20:14:50 +0200

    refactor: moving update check funcs to new file

---
+++ check_file_update.py
@@ -0,0 +1,51 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    check_file_update.py                               :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: zpalotas <zpalotas@42vienna.at>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/05/01 20:13:26 by zpalotas          #+#    #+#              #
+#    Updated: 2026/05/01 20:13:42 by zpalotas         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import os
+	# interact with your operating system (e.g. read env variables)
+import hashlib
+	# Python's built in hashing library
+import json
+	# reads and writes JSON files (key-value storage)
+
+# ```
+# Opening the file, reading it's bytes and producing a hash
+# ```
+def get_file_hash(filepath):							# opens in read binary mode (raw bytes instead of text bc pdf can get corroupted if read as text)
+	with open(filepath, "rb") as doc:					# "with" closes file when done or crash
+		hash_object = hashlib.md5(doc.read())			# hashlib.md5 is the same as uuid but uuid expects a string whereas this one wants raw bytes
+		readable_hash_object = hash_object.hexdigest()	# converts the object to readable str
+		return readable_hash_object
+
+# ```
+# Checks if the file has been processed already
+# searches the json file
+# returns the stored or (if not found) newly created hash 
+# ```
+def is_already_processed(filepath, hash_store):
+	if os.path.exists(hash_store):					# check if my json file exist
+		with open(hash_store, "r") as store_file:	# if exist open as "read only"
+			processed = json.load(store_file)		# convert json to python readable object, and load the hashes that exist
+	else:
+		processed = {}								# if doesnt exist create instead of crashing
+
+	file_hash = get_file_hash(filepath)				# call my func to get the unique hash if there was a prev version already
+
+	return file_hash in processed.values(), file_hash, processed # processed.values(): True/False, has it been seen before
+
+# ```
+# Saving the new hash, rewriting or creating entry
+# ```
+def mark_as_processed(filepath, file_hash, processed, hash_store):
+	processed[filepath] = file_hash				# rewrites or adds new entry: filename is key, hash is value
+	with open(hash_store, "w") as store_file:	# open to write
+		json.dump(processed, store_file)		# convert back to json. the reverse of json.load
--- data_extraction.py
+++ data_extraction.py
@@ -6,7 +6,7 @@
 #    By: zpalotas <zpalotas@42vienna.at>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/04/05 20:01:51 by zpalotas          #+#    #+#              #
-#    Updated: 2026/05/01 20:08:31 by zpalotas         ###   ########.fr        #
+#    Updated: 2026/05/01 20:13:17 by zpalotas         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,10 +15,6 @@
 
 import os
 	# interact with your operating system (e.g. read env variables)
-import hashlib
-	# Python's built in hashing library
-import json
-	# reads and writes JSON files (key-value storage)
 from dotenv import load_dotenv
 	# reads your API key from a .env file
 from IPython.display import display
@@ -26,51 +22,12 @@ from IPython.display import display
 
 # MY FUNCTIONS
 from processing_files	import processing_new_document
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Checking hash of the documents
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ```
-# The file which stores all the hashes
-# ```
-hash_store = "processed_files.json"
-
-# ```
-# Opening the file, reading it's bytes and producing a hash
-# ```
-def get_file_hash(filepath):							# opens in read binary mode (raw bytes instead of text bc pdf can get corroupted if read as text)
-	with open(filepath, "rb") as doc:					# "with" closes file when done or crash
-		hash_object = hashlib.md5(doc.read())			# hashlib.md5 is the same as uuid but uuid expects a string whereas this one wants raw bytes
-		readable_hash_object = hash_object.hexdigest()	# converts the object to readable str
-		return readable_hash_object
-
-# ```
-# Checks if the file has been processed already
-# searches the json file
-# returns the stored or (if not found) newly created hash 
-# ```
-def is_already_processed(filepath, hash_store):
-	if os.path.exists(hash_store):					# check if my json file exist
-		with open(hash_store, "r") as store_file:	# if exist open as "read only"
-			processed = json.load(store_file)		# convert json to python readable object, and load the hashes that exist
-	else:
-		processed = {}								# if doesnt exist create instead of crashing
-
-	file_hash = get_file_hash(filepath)				# call my func to get the unique hash if there was a prev version already
-
-	return file_hash in processed.values(), file_hash, processed # processed.values(): True/False, has it been seen before
-
-# ```
-# Saving the new hash, rewriting or creating entry
-# ```
-def mark_as_processed(filepath, file_hash, processed, hash_store):
-	processed[filepath] = file_hash				# rewrites or adds new entry: filename is key, hash is value
-	with open(hash_store, "w") as store_file:	# open to write
-		json.dump(processed, store_file)		# convert back to json. the reverse of json.load
+from check_file_update	import is_already_processed, mark_as_processed
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Loading the documents
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+hash_store = "processed_files.json"
 pdf_directory = os.fsdecode("data/TIN_description_pdfs/")
 for file in os.listdir(pdf_directory):
 	filepath = pdf_directory + file
